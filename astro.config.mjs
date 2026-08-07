6c550b7f08e8b0cf50f0ceb4eb7eaf488ea5d718
Aitorias <203893417+aitoriasdev@users.noreply.github.com>
2026-08-07 16:11:15 +0100

    feature: Added tailwind and created the header

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://aitoriasdev.github.io',
-    integrations: [sitemap()]
+  integrations: [sitemap()],
+  site: 'https://aitoriasdev.github.io',
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
