f0dce7090ff717291126861d8f57899181a8ebf1
Aitorias <203893417+aitoriasdev@users.noreply.github.com>
2026-08-11 15:10:42 +0100

    fonts: Added open dyslexic font for dyslexia

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,49 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 import partytown from "@astrojs/partytown";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  fonts: [
+    {
+      provider: fontProviders.local(),
+      name: "OpenDyslexic",
+      cssVariable: "--font-open-dyslexic",
+      options: {
+        variants: [
+          {
+            src: ["./src/assets/fonts/OpenDyslexic/OpenDyslexic-Regular.otf"],
+            weight: "normal",
+            style: "normal",
+          },
+          {
+            src: ["./src/assets/fonts/OpenDyslexic/OpenDyslexic-Bold.otf"],
+            weight: "bold",
+            style: "normal",
+          },
+          {
+            src: ["./src/assets/fonts/OpenDyslexic/OpenDyslexic-Italic.otf"],
+            weight: "normal",
+            style: "italic",
+          },
+          {
+            src: ["./src/assets/fonts/OpenDyslexic/OpenDyslexic-BoldItalic.otf"],
+            weight: "bold",
+            style: "italic",
+          },
+        ],
+      },
+    },
+  ],
   integrations: [
     partytown({
       config: {
-        forward: ['dataLayer.push'],
+        forward: ["dataLayer.push"],
       },
     }),
-    sitemap()
+    sitemap(),
   ],
   site: "https://aitoriasdev.github.io",
   vite: {
