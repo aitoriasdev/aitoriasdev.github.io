88433c4eae981a8d10a4273437742060cdd965eb
Aitorias <203893417+aitoriasdev@users.noreply.github.com>
2026-08-11 12:29:13 +0100

    seo: Added Google Analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,19 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import partytown from "@astrojs/partytown";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [sitemap()],
+  integrations: [
+    partytown({
+      config: {
+        forward: ['dataLayer.push'],
+      },
+    }),
+    sitemap()
+  ],
   site: "https://aitoriasdev.github.io",
   vite: {
     plugins: [tailwindcss()],
